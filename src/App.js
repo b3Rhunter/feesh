c30da7eabec2568fc114b93289fdcda268d41586
Pub <96320073+b3Rhunter@users.noreply.github.com>
2022-06-11 14:28:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -214,7 +214,7 @@ function App() {
             <s.TextTitle
               style={{
                 textAlign: "center",
-                fontSize: 50,
+                fontSize: 30,
                 fontWeight: "bold",
                 color: "var(--accent-text)",
               }}
